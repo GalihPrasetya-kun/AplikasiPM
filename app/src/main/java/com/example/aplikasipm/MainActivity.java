fdbb0ec5f018055e6970640d32632429a3bff1f3
Galih Prasetya Widodo <63995157+GalihPrasetya-kun@users.noreply.github.com>
2022-04-18 11:49:27 +0700

    Commit

--- app/src/main/java/com/example/aplikasipm/MainActivity.java
+++ app/src/main/java/com/example/aplikasipm/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.aplikasipm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnTambah, btnPenerima, btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnTambah = findViewById(R.id.btn_tambah);
+        btnPenerima = findViewById(R.id.btn_penerima);
+        btnLogout = findViewById(R.id.btn_logout);
+
+        btnTambah.setOnClickListener(v ->{
+
+        });
+        btnPenerima.setOnClickListener(v ->{
+
+        });
+        btnLogout.setOnClickListener(v ->{
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+        });
     }
 }
